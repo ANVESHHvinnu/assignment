17ec82ec419b7aa6a5ff261bf58053cc541759ff
ANVESHHvinnu <94430870+ANVESHHvinnu@users.noreply.github.com>
2021-11-16 11:27:06 +0530

    day 1 assignment done

---
+++ DAY 1 ASSIGNMENT PYTHON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = 'anvesh'
+age  = 23
+place= 'Hyderabad'
+
+
+# In[3]:
+
+
+print("my name is {}, my age is {}, and I live at {}" .format(name,age,place))
+
+
+# In[4]:
+
+
+print(f"2+3 is {2+3}")
+
+
+# In[5]:
+
+
+print("my age is {1}, my name is {0}".format(name,age))
+
+
+# In[ ]:
+
+
+
+

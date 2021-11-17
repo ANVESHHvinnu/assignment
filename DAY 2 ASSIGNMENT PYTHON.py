35b2a1a672156ab6be81c69fc4c6406ba429ae7c
ANVESHHvinnu <94430870+ANVESHHvinnu@users.noreply.github.com>
2021-11-17 18:04:43 +0530

    Add files via upload

---
+++ DAY 2 ASSIGNMENT PYTHON.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name = "anvesh"
+age  = 23
+percentage = 64.5
+result = True
+
+
+# In[4]:
+
+
+type(name)
+
+
+# In[5]:
+
+
+type(result)
+
+
+# In[6]:
+
+
+items = [1,2,3,3]
+keyvalues = {'name':'abc', 'age':23}
+values = {1,2,3,4}
+
+
+# In[7]:
+
+
+type(values)
+
+
+# In[8]:
+
+
+type(keyvalues)
+
+
+# In[9]:
+
+
+age*2
+
+
+# In[10]:
+
+
+age1 = '23'
+age1*2
+
+
+# In[12]:
+
+
+int(age)*2
+
+
+# In[14]:
+
+
+total_items= list()
+
+
+# In[15]:
+
+
+range(5)
+
+
+# In[16]:
+
+
+list(range(5))
+
+
+# In[20]:
+
+
+for items in range(5):
+   input()
+
+
+# In[21]:
+
+
+total_items.append(input())
+
+
+# In[23]:
+
+
+total_items
+
+
+# In[25]:
+
+
+total_items.append(input())
+
+
+# In[26]:
+
+
+total_items
+
+
+# In[29]:
+
+
+tot= [1,2,3,4,'anvesh',()]
+print(tot)
+
+
+# In[31]:
+
+
+tot.insert(0,'none')
+
+
+# In[32]:
+
+
+print(tot)
+
+
+# In[33]:
+
+
+tota=[3,4,5]
+
+
+# In[36]:
+
+
+tot.extend(tota)
+
+
+# In[37]:
+
+
+print(tot)
+
+
+# In[42]:
+
+
+now = (1,2,3,4,[])
+
+
+# In[43]:
+
+
+type(now)
+
+
+# In[44]:
+
+
+now[4].append('anvesh')
+
+
+# In[45]:
+
+
+now
+
+
+# In[ ]:
+
+
+
+
